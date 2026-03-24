87cc9b42c1a6400d6bee5aebddd8e6f38f6e3f87
Diana <dfranciscamagalhaes@gmail.com>
2026-03-24 23:53:52 +0000

    Initial commit

---
+++ Assets/Change Background Script.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeRawImageTexture : MonoBehaviour
+{
+    public RawImage rawImage;   // Assign in Inspector
+    public Texture newTexture;  // Assign the texture you want
+
+     public void ChangeTexture()
+    {
+        if (rawImage != null && newTexture != null)
+        {
+            rawImage.texture = newTexture;
+        }
+    }
+}
---
+++ Assets/Piece Script.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class NewMonoBehaviourScript : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler 
+{
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+    }
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("Clicked");
+        }
+
+}
